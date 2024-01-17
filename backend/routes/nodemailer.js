6e9745ad861f84e50e073a44a74824ea22b8435d
Ankit Pratap <123142821+royalrajput04@users.noreply.github.com>
2024-01-18 00:13:32 +0530

    .

--- backend/routes/nodemailer.js
+++ backend/routes/nodemailer.js
@@ -6,10 +6,10 @@ const {google} = require("googleapis");
 const { gmail } = require("googleapis/build/src/apis/gmail");
 
 
-const CLIENT_ID = '460493599138-nqmt3ppr4nkdnigv73318k5fq1uic8f9.apps.googleusercontent.com'
-const CLIENT_SECRET = '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'
+const CLIENT_ID = '460493599138-c7v9nv0f3dlvi3lfq276t7671hi0vvu0.apps.googleusercontent.com'
+const CLIENT_SECRET = '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'
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-const REFRESH_TOKEN = '1//0467R2Cw_xJrnCgYIARAAGAQSNwF-L9IrkNXakvFj2zsQn8OhpV_bVdBdXaQ2noPbSyn8ivDj8fc1qx6K-SoVJ7rNCxILLGXru9o'
+const REFRESH_TOKEN = '1//0439N97cC_eF1CgYIARAAGAQSNwF-L9IrA8hvbIxljHdZv0dsA0ein8v4ddI3etfNiN52nnoKvykEvIdQKjkMkjkSDA1h6DVahSQ'
 // const OAuth2 = google.auth.OAuth2;
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
@@ -30,7 +30,7 @@ router.post("/allotmentmail/:id", async (req,res)=>{
            console.log(req.params.id)
         }).catch(err=>{console.log(err)})
         let accessToken = await oAuth2Client.getAccessToken()
-        // console.log(oAuth2Client)
+        console.log(oAuth2Client)
         
         let transport = nodemailer.createTransport({
             service: 'gmail',
@@ -46,12 +46,16 @@ router.post("/allotmentmail/:id", async (req,res)=>{
                 rejectUnauthorized:false
             }
           });
-        //   console.log(transport)
-        //   let p =0;
-        //   if(participant.institute == "NIT Durgapur"){
-        //        p = 600;
-        //   }
-        //   else  p = 1300;
+          console.log(transport)
+          let p =0;
+          if(participant.institute == "NIT Durgapur"){
+               p = 600;
+          }
+          else  {
+            p = 1300;
+          }
+
+          console.log("papa..........")
 
         const main = async() => {
             console.log("sending email........")
@@ -60,9 +64,9 @@ router.post("/allotmentmail/:id", async (req,res)=>{
             to: participant.email, // dada niket email testing
             subject: "Registration confirmation", 
             text: ``, 
-            html: `Greetings <b>${participant.name}</b>,<br/><br/>Following your registration in <b>NITMUN XI</b>, you are requested to submit a registration fee of <b>Rs ${p}</b>.<br/>You may pay using UPI to the following people (UPI IDs provided below) :<br/><br/><b>Vedang Chauhan</b> -vedangc03@oksbi
+            html: `Greetings <b>${participant.name}</b>,<br/><br/>Following your registration in <b>NITMUN XII</b>, you are requested to submit a registration fee of <b>Rs ${p}</b>.<br/>You may pay using UPI to the following people (UPI IDs provided below) :<br/><br/><b>Vedang Chauhan</b> -vedangc03@oksbi
             (+91 90279 79974)<br/><b>Poorab Kumar</b> - poorab.kumar@paytm 
-            (+91 70639 67246)<br/><br/>Please mention NITMUN XI- ( your name ) - ( institution ) while sending it. <br/>Let us know when and to whom you have made the payment, via mail. Kindly <b>attach a screenshot</b> of the payment record to the email.<br/><br/>Regards,<br/>Navneet Berwal,<br/>Under Secretary General,<br/>NITMUN XI.<br/>Contact number -+91 85296 22552`, 
+            (+91 70639 67246)<br/><br/>Please mention NITMUN XII- ( your name ) - ( institution ) while sending it. <br/>Let us know when and to whom you have made the payment, via mail. Kindly <b>attach a screenshot</b> of the payment record to the email.<br/><br/>Regards,<br/>Navneet Berwal,<br/>Under Secretary General,<br/>NITMUN XI.<br/>Contact number - +91 85296 22552`, 
             
           });
           console.log("success")
@@ -120,7 +124,7 @@ router.post("/paymentmail/:id", async (req,res)=>{
             to: participant.email, // list of receivers
             subject: "Payment Confirmation ", 
             text: "",
-            html: `Dear <b>${participant.name}</b>,<br/><br/>Your payment has been received.We look forward to hosting you.<br/> <br/>Regards,<br/>Soumik Biswas,<br/>Deputy Director General,<br/>NITMUN XI.<br/>Contact - +916290575119.`, 
+            html: `Dear <b>${participant.name}</b>,<br/><br/>Your payment has been received.We look forward to hosting you.<br/> <br/>Regards,<br/>Soumik Biswas,<br/>Deputy Director General,<br/>NITMUN XII.<br/>Contact - +916290575119.`, 
            
           });
         // console.log("Message sent: %s", info.messageId);
